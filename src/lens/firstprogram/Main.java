8ce0975d48e8490a8526d2f4bf065d6bfad028ad
the-lens <gajewski.woj@gmail.com>
2019-10-30 10:25:28 +0100

    My first commit

--- src/lens/firstprogram/Main.java
+++ src/lens/firstprogram/Main.java
@@ -1,8 +1,13 @@
 package lens.firstprogram;
-//test komenarza
+//test komentarza
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Intellij!");
+        /*
+
+        Test drugi
+         */
+
 
 
     }
